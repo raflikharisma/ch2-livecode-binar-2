8c59cf55c50aa417416b7302b8e65692e65aedfa
raflikharismaa <raflikharisma29@webmail.umm.ac.id>
2024-03-05 22:08:00 +0700

    core: practice

---
+++ index.js
@@ -0,0 +1,13 @@
+var title = document.firstElementChild.lastElementChild.firstElementChild;
+
+// title.innerHTML = "Yeji ❤️ Rafli";
+// var list = document.getElementById("list");
+
+// for (var i = 0; i < 5; i++) {
+//     var newItem = document.createElement("li");
+//     newItem.textContent = "Item " + (i + 1); // Set the text content for the new <li> element
+//     list.appendChild(newItem); // Append the new <li> element to the list
+// }
+// var childList = document.querySelectorAll("li").classList;
+
+//document.querySelector("div h1").setAttribute("class", "bg-dark");
